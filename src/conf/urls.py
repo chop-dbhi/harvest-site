0a06e4a527af9a19351bb54d2c1d49a1d6b429cd
Byron Ruth <b@devel.io>
2012-01-11 13:14:35 -0500

    Add Get Started pages. Update more SCSS. Switch back to writing HTML

--- src/conf/urls.py
+++ src/conf/urls.py
@@ -1,12 +1,9 @@
 import re
-from django.conf.urls.defaults import url, patterns, include
+from django.conf.urls.defaults import url, patterns
 from django.conf import settings
 from django.contrib import admin
-from django.template import add_to_builtins
 
 admin.autodiscover()
-
-add_to_builtins('django.contrib.markup.templatetags.markup')
 
 urlpatterns = patterns('',
     # Poor man's flatpages..
