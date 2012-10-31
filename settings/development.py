4851840e3a96add7f9b2aae89fd425357f9cb563
Byron Ruth <b@devel.io>
2012-10-31 17:24:08 -0400

    Remove host-specific settings

--- settings/development.py
+++
@@ -1,8 +0,0 @@
-# Uncomment to put the application in non-debug mode. This is useful
-# for testing error handling and messages.
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-
-FORCE_SCRIPT_NAME = '/harvest-site'
-
-DATABASES = {'default': {'ENGINE': 'django.db.backends.dummy'}}
